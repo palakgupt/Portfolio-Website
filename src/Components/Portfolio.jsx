8d5a8f4a027a2bcc4a753c112e2d0ea8349ef6f9
palak <gpalak518@gmail.com>
2024-07-05 00:55:13 +0530

    name new changes

---
+++ src/Components/Portfolio.jsx
@@ -0,0 +1,86 @@
+/**
+ * Portfolio component
+ *
+ * Highlights some of  your creations. These can be designs, websites,
+ * open source contributions, articles you've written and more.
+ *
+ * This is a great area for you to to continually add to and refine
+ * as you continue to learn and create.
+ */
+
+import React from "react";
+
+/**
+ * Desk image
+ *
+ * Below is a sample desk image. Feel free to update this to an image of your choice,
+ * updating below imageAltText to string that represents what you see in that image.
+ *
+ * Need an image? Check out https://unsplash.com to download a photo you
+ * freely use on your site.
+ */
+import image from "../images/design-desk.jpeg";
+
+const imageAltText = "desktop with books and laptop";
+
+/**
+ * Project list
+ *
+ * An array of objects that will be used to display for your project
+ * links section. Below is a sample, update to reflect links you'd like to highlight.
+ */
+const projectList = [
+  {
+    title: "Blog Website",
+    description:
+      "A blog website is an online platform where individuals or organizations can publish articles and stories . Creating a blog website using Python and Django is a common and rewarding project for both beginners and experienced developers.",
+    url: "",
+  },
+  {
+    title: "Job Finder App",
+    description:
+      "Creating a job finder app with React.js involves building a dynamic, responsive, and user-friendly web application where users can search for job listings, apply for jobs, and manage their job applications. ",
+    url: "",
+  },
+  {
+    title: "My Portfolio Site",
+    description:
+      "Creating a portfolio site using React.js is a fantastic way to showcase your skills, projects, and experience.",
+    url: "",
+  },
+  {
+    title: "Blood Bank App",
+    description:
+      "Creating a Blood Bank App using the MERN stack (MongoDB, Express.js, React.js, and Node.js) is a comprehensive project that covers both front-end and back-end development.",
+    url: "",
+  },
+];
+
+const Portfolio = () => {
+  return (
+    <section className="padding" id="portfolio">
+      <h2 style={{ textAlign: "center" }}>Portfolio</h2>
+      <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
+        <div style={{ maxWidth: "40%", alignSelf: "center" }}>
+          <img
+            src={image}
+            style={{ height: "90%", width: "100%", objectFit: "cover" }}
+            alt={imageAltText}
+          />
+        </div>
+        <div className="container">
+          {projectList.map((project) => (
+            <div className="box" key={project.title}>
+              <a href={project.url} target="_blank" rel="noopener noreferrer">
+                <h3 style={{ flexBasis: "40px" }}>{project.title}</h3>
+              </a>
+              <p className="small">{project.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
